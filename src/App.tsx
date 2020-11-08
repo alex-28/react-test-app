bead27360f1e0d74cded92137e7262b1f8d655e9
alex-28 <66523402+alex-28@users.noreply.github.com>
2020-11-08 18:55:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, withRouter, RouteComponentProps } from 'react-router-dom';
+import { Menu as MenuAntd } from 'antd';
 
- const Item = () => (<div>item id</div>)
+const _Menu: React.FC<RouteComponentProps> = (props) => {
+  const menu = [
+    {
+      title: 'Home',
+      to: '/',
+    },
+    {
+      title: 'About',
+      to: '/about',
+    },
+    {
+      title: 'Items',
+      to: '/items',
+    },
+  ];
+  const menuItem = menu.find((item) => item.to === props.location.pathname);
+  return (
+    <MenuAntd selectedKeys={menuItem ? [menuItem.to] : []} mode="horizontal" theme="dark">
+      {menu.map((item) => (
+        <MenuAntd.Item key={item.to}>
+          <Link to={item.to}>{item.title}</Link>
+        </MenuAntd.Item>
+      ))}
+    </MenuAntd>
+  );
+};
 
-export function App() {
-  const home = '/';
-  const about = '/about';
+const Menu = withRouter(_Menu);
 
-  return (<div>
-    <Link to={home}>home</Link>
-    <Link to={about}>about</Link>
-    <Link to={'/items'}>items</Link>
-  <Route exact path={home} component={() => <div>Home {home}</div>} />
-    <Route exact path={about} component={() => <div>About {about}</div>} />
-    <Route exact path='/items' component={() => <div>items</div>} />
-      <Route exact path='/items/:id' component={Item} />
-  </div>)
-}
+const _Item: React.FC<RouteComponentProps<{ id: string }>> = (props) => {
+  return <div>{props.match.params.id} item</div>;
+};
+
+const Item = withRouter(_Item);
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <Menu />
+      <div style={{ fontSize: '20px' }}>
+        <Route exact path="/" component={() => <h1>Home page</h1>} />
+        <Route exact path="/about" component={() => <h1>About page</h1>} />
+        <Route
+          exact
+          path="/items"
+          component={() => (
+            <div>
+              {[1, 2, 3].map((item) => (
+                <Link to={`items/${item}`}>{item}</Link>
+              ))}
+            </div>
+          )}
+        />
+        <Route exact path="/items/:id" component={Item} />
+      </div>
+    </div>
+  );
+};
